56c63e6ce5ceb197ef3cf5376f17e3a715a33ae5
DonPepino30 <131565147+DonPepino30@users.noreply.github.com>
2024-05-06 16:35:16 -0500

    Delete Python_Clase4 directory

--- Python_Clase4/Prueba1.py
+++
@@ -1,9 +0,0 @@
-def mayor(a,b,c,d):
-    if(a>b & a>c & a>d):
-        return a
-    if(b>a & b>c & b>d):
-        return b
-    if(c>a & c>b & c>d):
-        return c
-    else: return d
-print(mayor(int(input("Ingrese un numero a:")), int(input("Ingrese un numero b: ")), int(input("Ingrese un numero c:")), int(input("Ingrese un numero d: ")))**3)
--- Python_Clase4/Prueba2.py
+++
@@ -1,6 +0,0 @@
-def numeros(a,b):
-    if a%b==0:
-        print("a es multiplo de b")
-    if b%a==0:
-        print("b es multiplo de a")
-print(numeros(int(input("Ingrese un numero a:")), int(input("Ingrese un numero b: "))))
