29f2592c36e3a74d1d22981257f986923a879a7c
DonPepino30 <131565147+DonPepino30@users.noreply.github.com>
2024-05-09 14:01:16 -0500

    Update Ejercicio12.py

--- Reforzamiento4/Ejercicio12.py
+++ Reforzamiento4/Ejercicio12.py
@@ -3,3 +3,19 @@ ejecutar una función a ser decorada y luego de ser ejecutada lanzará
 un mensaje diciendo “Hasta luego”.
 La función a decorar pedirá el nombre de una persona y retornará el
 mensaje “Soy ‘nombre’”."""
+
+def saludo(fun):
+    def saludoInterno():
+        print("Buenos días!")
+        nombre = input("Cuál es tu nombre? ")
+        resultado = fun(nombre)
+        print("Hasta luego!")
+        return resultado
+
+    return saludoInterno
+
+@saludo
+def saludarPersona(nombre):
+    return f"Soy {nombre}"
+
+print(saludarPersona())
