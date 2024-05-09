70071e3c3657be6ba6cf77f79fffe895fa117617
DonPepino30 <131565147+DonPepino30@users.noreply.github.com>
2024-05-09 14:17:21 -0500

    Update Ejercicio14.py

--- Reforzamiento4/Ejercicio14.py
+++ Reforzamiento4/Ejercicio14.py
@@ -3,3 +3,16 @@ tiene la función a decorar usando *args y **kwargs.
 Mensaje previo “La cantidad de argumentos que tiene la función es”
 mensaje post “La función decoradora terminó de ejecutarse
 correctamente”"""
+
+def contadorArgs(func):
+    def funcA(*args, **kwargs):
+        print(f"La cantidad de argumentos que tiene la función es {len(args) + len(kwargs)}")
+        resultado = func(*args, **kwargs)
+        print("La función decoradora terminó de ejecutarse correctamente.")
+        return resultado
+    return funcA
+
+@contadorArgs
+def multiplicar(a, b, c, d):
+    return a * b * c * d
+print(multiplicar(2, 3, 5, 8))
