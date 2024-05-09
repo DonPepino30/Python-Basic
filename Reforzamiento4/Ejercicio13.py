5f4d11d3b369cd3282f235349cccd4dfc495a2cd
DonPepino30 <131565147+DonPepino30@users.noreply.github.com>
2024-05-09 14:08:12 -0500

    Update Ejercicio13.py

--- Reforzamiento4/Ejercicio13.py
+++ Reforzamiento4/Ejercicio13.py
@@ -4,3 +4,21 @@ multiplicaran entre ellos.
 Mensaje previo al usar el decorador “Está por ejecutarse la función” y
 mensaje luego de usar el decorador “La función ha sido ejecutado
 correctamente”"""
+
+def multiplicadorD(*args):
+    def funcB(func):
+        def funcC(*kwargs):
+            print("Está por ejecutarse la función")
+            resultado = func(*args, *kwargs)
+            print("La función ha sido ejecutada correctamente")
+            return resultado
+
+        return funcC 	
+
+    return funcB
+
+@multiplicadorD(4, 5, 6, 7)
+def multiplicar(*num):
+    return num[0] * num[1] * num[2] * num[3]
+
+print(multiplicar(3, 2, 1, 0))
